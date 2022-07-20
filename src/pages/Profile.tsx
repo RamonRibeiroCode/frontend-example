f06e60e5bf77908982e651033c1f45b1371b7079
Ramon Ribeiro <ramon_ramos_ext@carrefour.com>
2022-07-19 23:30:44 -0300

    feat: cria pagina profile

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,74 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AxiosError } from "axios";
+
 import AuthContext from "../contexts/auth";
+import api from "../services/api";
+
+interface Profile {
+  age: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  photos: ProfilePhoto[];
+}
+
+interface ProfilePhoto {}
 
 const Profile = () => {
-  const { user, handleLogout } = useContext(AuthContext);
+  const [profile, setProfile] = useState({} as Profile);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const { handleLogout } = useContext(AuthContext);
+
+  const getProfile = async () => {
+    try {
+      const response = await api.get("/users/profile");
+
+      setProfile(response.data);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        setError(error.response?.data.message);
+      } else {
+        setError("Unexpected Error");
+      }
+    }
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
+
+  if (loading) {
+    return <span>Loading...</span>;
+  }
+
+  if (error) {
+    return <span>{error}</span>;
+  }
 
   return (
     <div>
-      <h1>Ola {user.name}</h1>
+      <table>
+        <tr>
+          <td>Years:</td>
+          <td>{profile.age}</td>
+        </tr>
+        <tr>
+          <td>Email:</td>
+          <td>{profile.email}</td>
+        </tr>
+        <tr>
+          <td>First Name:</td>
+          <td>{profile.firstName}</td>
+        </tr>
+        <tr>
+          <td>Last Name:</td>
+          <td>{profile.lastName}</td>
+        </tr>
+      </table>
 
       <button onClick={handleLogout}>Logout</button>
     </div>
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,4 +4,20 @@ const api = axios.create({
   baseURL: "http://localhost:3333",
 });
 
+api.interceptors.request.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401) {
+      localStorage.clear();
+      window.location.replace("/");
+
+      return;
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default api;
